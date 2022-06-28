e84084881f8d89c1d8d25ac9823047980b997dd4
eman-hesham97 <eng.eman.eltaweel@gmail.com>
2022-06-28 13:04:12 +0200

    Main Page

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/ParkCard.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
@@ -10,10 +11,157 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  // Widget build(BuildContext context) {
+  //   return new Scaffold(
+  //     body: new Center(
+  //       child: new Text("This is all parkings info page"),
+  //     ),
+  //   );
+  // }
+
+  @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Center(
-        child: new Text("This is all parkings info page"),
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+
+      body: Stack(
+        children: <Widget>[
+          // ignore: unnecessary_new
+          new Container(
+              // width: 375,
+              height: 812,
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0, top: 5),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  ),
+                ),
+              ),
+              // ignore: unnecessary_new
+              decoration: new BoxDecoration(
+                  borderRadius: BorderRadius.circular(31),
+
+              )
+          ),
+          Positioned(
+            top: 10,
+            // ignore: unnecessary_new
+            child: new Container(
+                width: 500,
+                height: 400,
+                child: Column(
+                  children: <Widget>[
+                    Flexible(
+                      child: ListView(
+                        scrollDirection: Axis.vertical,
+                        children: <Widget>[
+                          // ++++++++++++++++++++++++++++++++++++++++ PARK 1 ++++++++++++++++++++++++++++++++++++++++++++
+                          Row(children: <Widget>[
+                            Container(
+                              child: ParkCard('Nasr City', 'Nasr City', true,),
+                            ),
+                            Container(
+                              margin: const EdgeInsets. only(top: 58.0),
+                              child: FlatButton( 
+                                splashColor: Colors.green, 
+                                height: 70,
+                                minWidth: 155,
+                              child: Text('View Details', style: TextStyle(fontSize: 15.0),),  
+                              onPressed: () {},  
+                            ),
+                            decoration: new BoxDecoration(
+                              // borderRadius: BorderRadius.circular(8),
+                              gradient: LinearGradient(colors: [
+                                Colors.deepPurple.shade50,
+                                Colors.deepPurple.shade50 ],
+                                  stops: [
+                                    0,
+                                    1
+                                  ]
+                              )
+                          )
+                            )
+                          ]),
+                          // ParkCard('Parking 1', 'Nasr City', true,),
+                          // ++++++++++++++++++++++++++++++++++++++++ PARK 2 ++++++++++++++++++++++++++++++++++++++++++++
+                          Row(children: <Widget>[
+                            Container(
+                              margin: const EdgeInsets. only(top: 58.0),
+                              child: FlatButton( 
+                                splashColor: Colors.green, 
+                                textColor: Colors.white,
+                                height: 70,
+                              child: Text('View Details', style: TextStyle(fontSize: 15.0),),  
+                              onPressed: () {},  
+                            ),
+                            decoration: new BoxDecoration(
+                              // borderRadius: BorderRadius.circular(8),
+                              gradient: LinearGradient(colors: [
+                                Colors.deepPurple.shade700,
+                                Colors.deepPurple.shade700 ],
+                                  stops: [
+                                    0,
+                                    1
+                                  ]
+                              )
+                          )
+                            ),
+                          Container(
+                              child: ParkCard('Fifth Settlement', 'Fifth Settlement', false,),
+                            ), 
+                          ]),
+                          // ParkCard('Parking 2', 'Fifth Settlement', false,),
+                          // ++++++++++++++++++++++++++++++++++++++++ PARK 3 ++++++++++++++++++++++++++++++++++++++++++++
+                          Row(children: <Widget>[
+                            Container(
+                              child: ParkCard('Maadi', 'Maadi', true, ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets. only(top: 58.0),
+                              child: FlatButton( 
+                                splashColor: Colors.green, 
+                                height: 70,
+                                minWidth: 155,
+                              child: Text('View Details', style: TextStyle(fontSize: 15.0),),  
+                              onPressed: () {},  
+                            ),
+                            decoration: new BoxDecoration(
+                              // borderRadius: BorderRadius.circular(8),
+                              gradient: LinearGradient(colors: [
+                                Colors.deepPurple.shade50,
+                                Colors.deepPurple.shade50 ],
+                                  stops: [
+                                    0,
+                                    1
+                                  ]
+                              )
+                          )
+                            )
+                          ]),
+
+                          // ParkCard('Parking 3', 'Maadi', true, ),
+                          // ParkCard('Maruti Swift', 'Rs.4.99 - 8.85 Lakh*', 'assets/carlist4.png', 'assets/background_left.png', false,)
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                // ignore: unnecessary_new
+                decoration: new BoxDecoration(
+                    // color: Color(0xffeff5ff),
+                    borderRadius: BorderRadius.circular(32),
+                )
+            ),
+          )
+
+        ],
       ),
     );
   }
---
+++ lib/ParkCard.dart
@@ -0,0 +1,186 @@
+// ignore_for_file: unnecessary_new
+
+import 'package:flutter/material.dart';
+
+class ParkCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  // final String image;
+  // final String background;
+  final bool isOdd;
+
+  ParkCard(
+      this.title,
+      this.subtitle,
+      // this.image,
+      // this.background,
+      this.isOdd,
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 130,
+      width: 351,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 5,
+            right: !isOdd ? 20 : 0,
+            left: !isOdd ? 0 : 70,
+            // ignore: unnecessary_new
+            child: new Container(
+                width: 351,
+                height: 62,
+                // ignore: unnecessary_new
+                decoration: new BoxDecoration(
+                    // borderRadius: r,
+                    gradient: LinearGradient(colors: [
+                      Colors.deepPurple.shade700,
+                      Colors.deepPurple.shade50 ],
+                        stops: [
+                          0,
+                          1
+                        ]
+                    )
+                )
+            ),
+          ),
+
+          isOdd ?
+              Positioned(
+                left: 60,
+                right: 0,
+                bottom: 20,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Text(title,
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Color(0xffffffff),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.normal,
+                          letterSpacing: 0,
+
+                        )
+                    ),
+                    new Text(subtitle,
+                        style: TextStyle(
+                          fontFamily: 'AlegreyaSansSC',
+                          color: Color(0xffffffff),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          letterSpacing: 0,
+
+                        )
+                    )
+
+                  ],
+                ),
+              )
+          :
+            Positioned(
+              bottom: 20,
+              right: 60,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Text(title,
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        color: Color(0xffffffff),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.normal,
+                        letterSpacing: 0,
+
+                      )
+                  ),
+                  new Text(subtitle,
+                      style: TextStyle(
+                        fontFamily: 'AlegreyaSansSC',
+                        color: Color(0xffffffff),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.italic,
+                        letterSpacing: 0,
+
+                      )
+                  )
+                ],
+              ),
+            ),
+
+          // // Background
+          // isOdd ?
+          // Positioned(
+          //   right: 5,
+          //   top: 20,
+          //   child: Container(
+          //     height: 92,
+          //     width: 200,
+          //     decoration: BoxDecoration(
+          //         // image: DecorationImage(
+          //         //     image: AssetImage(background),
+          //         //     fit: BoxFit.cover
+          //         // )
+          //     ),
+          //   ),
+          // )
+          //     :
+          // Positioned(
+          //   left: 5,
+          //   top: 20,
+          //   child: Container(
+          //     height: 92,
+          //     width: 200,
+          //     decoration: BoxDecoration(
+          //         // image: DecorationImage(
+          //         //     image: AssetImage(background),
+          //         //     fit: BoxFit.cover
+          //         // )
+          //     ),
+          //   ),
+          // ),
+
+
+          // isOdd ?
+          //   Positioned(
+          //   right: 20,
+          //   top: 20,
+          //   child: Container(
+          //     height: 92,
+          //     width: 152,
+          //     decoration: BoxDecoration(
+          //       // image: DecorationImage(
+          //       //     image: AssetImage(image),
+          //       //     fit: BoxFit.cover
+          //       // )
+          //     ),
+          //   ),
+          // )
+          //   :
+          //   Positioned(
+          //   left: 20,
+          //   top: 20,
+          //   child: Container(
+          //     height: 92,
+          //     width: 152,
+          //     decoration: BoxDecoration(
+          //         // image: DecorationImage(
+          //         //     image: AssetImage(image),
+          //         //     fit: BoxFit.cover
+          //         // )
+          //     ),
+          //   ),
+          //   )
+
+        ],
+      ),
+
+    );
+  }
+}
